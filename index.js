cbc1c5e4183195ad1a1148940bf10a2669e781fd
Denner Soares <denner@_DennerMaria_31git config --global user.name denner>
2025-06-05 16:40:23 -0300

    Adicionar funcionalidade de LocalStorage a Lista de Tarefas

--- index.js
+++ index.js
@@ -2,24 +2,48 @@ const input = document.getElementById('inputTarefa');
 const botao = document.getElementById('btnAdicionar');
 const lista = document.getElementById('listaTarefas');
 
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+
+function salvarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+
+function renderizarTarefas() {
+    lista.innerHTML = '';
+    tarefas.forEach((texto, index) => {
+        const item = document.createElement('li');
+        item.textContent = texto;
+
+        item.addEventListener('click', () => {
+            item.classList.toggle('concluida');
+        });
+
+        const btnRemover = document.createElement('button');
+        btnRemover.textContent = 'Remover';
+        btnRemover.style.margin = "10px"
+        btnRemover.addEventListener('click', () => {
+            tarefas.splice(index, 1);
+            salvarTarefas();
+            renderizarTarefas();
+        });
+
+        item.appendChild(btnRemover);
+        lista.appendChild(item);
+    });
+}
+
+
 botao.addEventListener('click', () => {
     if (input.value.trim() === '') return;
 
-    const item = document.createElement('li');
-    item.textContent = input.value;
-
-    item.addEventListener('click', () => {
-        item.classList.toggle('concluida');
-    });
-
-    const btnRemover = document.createElement('button');
-    btnRemover.textContent = 'Remover';
-    btnRemover.style.margin = "10px"
-    btnRemover.addEventListener('click', () => {
-        item.remove();
-    });
-
-    item.appendChild(btnRemover);
-    lista.appendChild(item);
+    tarefas.push(input.value.trim());
+    salvarTarefas();
+    renderizarTarefas();
     input.value = '';
 });
+
+
+renderizarTarefas();
